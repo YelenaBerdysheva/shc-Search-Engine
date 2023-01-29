456a7f8bc1d5d5d21287fa5cbc8d8b4945cfa6f2
YelenaBerdysheva <90384657+YelenaBerdysheva@users.noreply.github.com>
2023-01-29 18:59:25 +0200

    Units

--- src/index.js
+++ src/index.js
@@ -5,58 +5,9 @@ let temperatureGl;
 
 initialRender();
 
-// // dislay current date
-// let currentDay = days[currentTime.getDay()];
-// let hours = currentTime.getHours();
-// let minutes = currentTime.getMinutes();
-// let formatDate = ` ${currentDay} ${hours}:${minutes}`;
-// let dateEl = document.querySelector("#date");
-// dateEl.innerHTML = formatDate;
-
-// function onSubmit(event) {
-//   event.preventDefault();
-
-//   // display city
-//   let searchEl = document.querySelector("#search");
-//   let city = searchEl.value;
-//   let h1El = document.querySelector("h1");
-//   h1El.innerHTML = city;
-
-//   // fetch temperature
-//   // dispay temperature
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
-//   axios.get(apiUrl).then(showWeather);
-// }
-
-// let submitEl = document.querySelector("#submit");
-// submitEl.addEventListener("click", onSubmit);
-
-// function showTemperature(response) {
-//   let temperature = Math.round(response.data.main.temp);
-//   let temperatureEl = document.querySelector("#temperature");
-//   temperatureEl.innerHTML = `${temperature}° ${response.data.name}`;
-// }
-
-// function showWeather(response) {
-//   showTemperature(response);
-//   // let descriptionEl = document.querySelector("description");
-//   // console.log(1111);
-//   // descriptionEl.innerHTML = `${response.data.weather[0].description}`;
-// }
-// function showIcon(responce) {
-//   let iconEl = document.querySelector("#icon");
-//   iconEl.setAttribute(
-//     "src",
-//     `https://api.openweathermap.org/img/wn/${responce.data[0].icon}04d@2x.png`
-//   );
-// }
-
 function initialRender() {
-  // dislay current date
   renderInitialDate();
 
-  // onSubmit handler
   let submitEl = document.querySelector("#submit-id");
   submitEl.addEventListener("click", onSubmit);
 
@@ -126,6 +77,7 @@ function renderIcon(response) {
 function renderTemperature(response) {
   let el = document.querySelector("#temperature-id");
   const temperature = Math.round(response.data.main.temp);
+  temperatureGl = response.data.main.temp;
   el.innerHTML = temperature;
 }
 
@@ -140,34 +92,9 @@ function renderWindSpeed(response) {
   el.innerHTML = `Wind: ${windSpeed} km/h`;
 }
 
-// --------------------------
-
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature-id");
-  temperatureElement.innerHTML = "fooo";
-  // celsiusLink.classList.remove("active");
-  // fahrenheitLink.classList.add("active");
-  // let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  // temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+  let fTemp = Math.round((temperatureGl * 9) / 5 + 32);
+  temperatureElement.innerHTML = fTemp;
 }
-
-// function displayCelsiusTemperature(event) {
-//   event.preventDefault();
-//   celsiusLink.classList.add("active");
-//   fahrenheitLink.classList.remove("active");
-//   let temperatureElement = document.querySelector("temperature");
-//   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-// }
-
-// let celsiusTemperature = null;
-
-// let form = document.querySelector("#search-form");
-// form.addEventListener("submit", handleSubmit);
-
-// let fahrenheitLinkEl = document.querySelector("#fahrenheit-link");
-// fahrenheitLinkEl.addEventListener("click", displayFahrenheitTemperature);
-
-// let celsiusLink = document.querySelector("#celsius-link");
-// celsiusLink.addEventListener("click", displayCelsiusTemperature);
-// search("New York");
